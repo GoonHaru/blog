86f723fe047c777b476be56647ff7a8d7a57e156
Goon <97924535+GoonHaru@users.noreply.github.com>
2022-04-27 20:56:18 +0630

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home"
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
           <Route path="/create" element={ <Create/> }>
           </Route>
           <Route path="/blogs/:id" element={ <BlogDetails/> }>
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Routes>
      </div>
